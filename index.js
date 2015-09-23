16bfff6e085d428ce80efbd4c569fcddfbea0818
Spencer B Liberto <spencerbliberto@gmail.com>
2015-09-22 21:50:57 -0500

    old players see new players

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var players = {}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/circles.html');
 });
 
 io.on('connection', function(socket){
-  socket.broadcast.emit('join')
-  console.log("join")
-  socket.on('chat message', function(msg, name){
-    socket.broadcast.emit('chat message', name + ": " + msg);
+  io.to(socket.id).emit('allplayers',JSON.stringify(players));
+  socket.on('join', function(id, coordinates){
+    console.log("join " + id)
+    socket.broadcast.emit('join', id, coordinates)
+    //io.to(socket.id).emit('allplayers', players)
+  })
+  socket.on('log', function(msg){
+    //console.log("log " + msg)
+  });
+  socket.on('move', function(id, x, y){
+    console.log("move " + id)
+    socket.broadcast.emit('move', id, x, y)
+    players[id] = {cx:x, cy:y}
   });
 });
 
